a870957cd3ef6761188ab6b635e0343cf1054afb
StarVoyar <starvoyar@gmail.com>
2026-08-31 15:08:35 +0100

    Neo tree saves width

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -1,5 +1,20 @@
+local state_file = vim.fn.stdpath("data") .. "/neo_tree_width.txt"
+local width = tonumber(vim.fn.readfile(state_file)[1]) or 28
+
 require("neo-tree").setup({
-  window = {
-    width = 28,
-  },
+  window = { width = width },
+})
+
+vim.api.nvim_create_autocmd("WinClosed", {
+  pattern = "*",
+  callback = function()
+    local win = tonumber(vim.fn.expand("<amatch>"))
+    if vim.api.nvim_win_is_valid(win) then
+      local buf = vim.api.nvim_win_get_buf(win)
+      if vim.bo[buf].filetype == "neo-tree" then
+        local w = vim.api.nvim_win_get_width(win)
+        vim.fn.writefile({ tostring(w) }, state_file)
+      end
+    end
+  end,
 })
